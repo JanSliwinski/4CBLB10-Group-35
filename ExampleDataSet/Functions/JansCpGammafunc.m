645da1f4576b004ab6f3501b6a6635b332246093
Kata <k.fulop@student.tue.nl>
2024-12-09 16:18:45 +0100

    again

---
+++ ExampleDataSet/Functions/JansCpGammafunc.m
@@ -0,0 +1,111 @@
+function [cp, gamma] = JansCpcode(LHV,mfr_fuel, mfr_air, T_int, AFR_stoich,)
+%% Find species
+% Locate indexes of specific species in the database
+iSp = myfind({Sp.Name}, {'O2', 'CO2', 'N2','H2O'}); % Find indexes of these species
+SpS = Sp(iSp); % Create subset of species based on found indexes
+NSp = length(SpS); % Number of species
+
+%% Given variables
+% Define mole fractions for each species
+% fractions represent typical air composition
+O2_frac = 0.1447;      % Oxygen fraction
+CO2_frac = 0.0467;    % Carbon dioxide fracitons
+N2_frac = 0.7808;      % Nitrogen fraction
+% Calculate water vapor fraciton by subtraction
+H2O_frac = 0.2095 - O2_frac - CO2_frac;
+CO_frac = 0;      % Carbon monoxide fraction
+
+% Combustion and thermal parameters
+% These should also be read from a table/structure.
+Q_dot = LHV * mfr_fuel; % Heat transfer rate (W)
+tolerance = 1e2;          % Acceptable error in heat transfer (W)
+deltaT = 100;             % Initial guess for temperature change (K)
+error = Inf;              % Initialize error to infinite
+
+% Calculate Air-Fuel Ratio (AFR)
+m_dot_tot = mfr_air + mfr_fuel; % Total mass flow rate
+
+%% Convert mole fractions to mass fractions
+
+% Collect mole fractions into an array
+moleFractions = [O2_frac, CO2_frac, N2_frac, H2O_frac];
+
+% Get molar masses of species
+Mi = [SpS.Mass]; % Molar masses in kg/mol
+
+% Calculate mass composition based on mole fractions and molar masses
+massComposition = (moleFractions .* Mi) / sum(moleFractions .* Mi);
+
+% Initialize counter for iteration tracking
+counter = 0;
+cpCum = []; % Array to potentially store cumulative cp values
+TCum = [];  % Array to potentially store cumulative temperatures
+
+%% Iterative calculation to find temperature change
+% Iterate until calculated heat transfer is within tolerance of given heat transfer
+while error > tolerance
+    % Calculate new temperature
+    T = T_int + deltaT;
+    
+    % Compute average temperature for cp calculation
+    T_avg = (T + T_int) / 2;
+    
+    % Calculate specific heat capacity at constant pressure
+    cp = compute_cp(T_avg, SpS, massComposition);
+    
+    % Calculate heat transfer based on current cp and temperature change
+    Q_dot_calculated = m_dot_tot * cp * deltaT;
+    
+    % Compute the error between calculated and given heat transfer
+    error = abs(Q_dot_calculated - Q_dot);
+    
+    % Increment temperature change and iteration counter
+    deltaT = deltaT + 0.1;
+    counter = counter + 1;
+
+    % for plotting
+    TCum = [TCum T_avg];
+    cpCum = [cpCum cp];
+end
+
+% Display success message and results
+disp("Great Success, cp = ");
+disp(cp);
+
+plot(TCum,cpCum)
+xlabel("Temperature")
+ylabel("C_p")
+title("c_p for post-combustion mixture vs temperature")
+subtitle("linear behaviour?")
+
+%% Repeated display of results
+disp("Great Success, cp = ");
+disp(cp);
+disp("Counter = ")
+disp(counter)
+disp("DeltaT = ")
+disp(deltaT)
+
+%% Find temperature where specific heat capacity becomes negative
+% Start at a high temperature
+T_test = 5000;
+cp_test = 5;
+
+% Decrease temperature until cp becomes negative
+while cp_test > 0
+    cp_test = compute_cp(T_test,SpS,massComposition);
+    T_test = T_test + 1;
+end
+
+% Compute cp at a temperature slightly below the negative cp point
+cp_show = compute_cp(T_test-2000,SpS,massComposition);
+
+% Display results of cp investigation
+fprintf("c_p for T > %i is negative! = %f \n", T_test, cp_test);
+fprintf("c_p for %i is %f \n", T_test-2000, cp_show);
+
+%% Compute heat capacity ratio (gamma)
+% Calculate ratio of cp to cv at maximum combustion temperature
+gamma = compute_cp(T,SpS,massComposition) / compute_cv(T,SpS,massComposition);
+
+end
