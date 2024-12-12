64757e274cd3f00e3d32f32c7cda00fd2e0c3c8a
Jan <j.j.sliwinski@student.tue.nl>
2024-12-12 21:40:11 +0100

    load 2... story continues

--- ExampleDataSet/pressureCalibration.m
+++ ExampleDataSet/pressureCalibration.m
@@ -1,5 +1,5 @@
 % Specify location of the experimental file(s) and open them
-folderPath = './Data/session1_Raw/load3.5';
+folderPath = './Data/session1_Raw/load2';
 outputfilePath = './Data/Processed_session1';
 
 % If averaging data also, use this code:
@@ -72,6 +72,6 @@ end
 averageData = sumData / numFiles;
 
 % Save averaged data
-outputFileName = fullfile(outputfilePath, 'averaged_filtered_data_3.5_IMEP.txt');
+outputFileName = fullfile(outputfilePath, 'averaged_filtered_data_2_IMEP.txt');
 disp(['Saving averaged data to: ', outputFileName]);
 writematrix(averageData, outputFileName);
